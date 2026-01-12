cc4b0d521dd4fc5a9c33a67b765bace596d48ad0
Ammarqasmi03 <amjadammar786786@gmail.com>
2026-01-12 16:20:07 +0530

    Add new files

--- Poll-App/README.md
+++ Poll-App/README.md
@@ -1,4 +1,4 @@
-# PollAppliaction
+# PollApp
 
 This project was generated using [Angular CLI](https://github.com/angular/angular-cli) version 21.0.5.
 
--- Poll-App/src/app/app.ts
+++ Poll-App/src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('Poll-Application');
+  protected readonly title = signal('Poll-App');
 }
